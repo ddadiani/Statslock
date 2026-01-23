0612f62068d4f6b18433bd36f6a53bbfb84d1990
dadian1 <daviddadiann@gmail.com>
2026-01-23 21:17:18 +0400

    added html main body section template and loadTemplate function in js

--- main.js
+++ main.js
@@ -3,17 +3,33 @@ const playerButton = document.getElementById("player-lookup-button")
 
 
 // listeners for clicking the buttons
+
+// hero lookup listener
 heroButton.addEventListener("click", () => {
     heroButton.classList.add("active")
     playerButton.classList.remove("active")
-    
-    const section = document.getElementById('main-content');
+
+    loadTemplate("hero-lookup")
 })
 
+// player lookup listener
 playerButton.addEventListener("click", () => {
     heroButton.classList.remove("active")
     playerButton.classList.add("active")
+
+    loadTemplate("player-lookup")
 })
+
+// function for loading appropriate lookup html content
+function loadTemplate(id) {
+    const main = document.getElementById('main-content');
+    const template = document.getElementById(id).cloneNode(true);
+    main.innerHTML = '';
+    main.appendChild(template.content);
+}
+
+
+
 
 // by default, make the hero lookup pressed and show
 heroButton.click()
